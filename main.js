d9cfc021efb068b4f5b141ce84bc501a5d27d9d6
baxtiyor <baxtiyorproweb4437@gmail.com>
2022-09-15 18:08:54 +0500

    Color Picker App

---
+++ main.js
@@ -0,0 +1,28 @@
+const imgInput = document.querySelector('#img-select');
+const imgPreview = document.querySelector('.preview');
+if (!window.EyeDropper) {
+    alert(`Sizning brauzeringiz bu xususiyatni qo'llab-quvvatlamaydi`)
+}
+
+const eyeDropper = new EyeDropper()
+const pickerBtn = document.querySelector('.open-picker');
+const result = document.querySelector('.res');
+imgInput.addEventListener('change', function () {
+    const file = this.files[0]
+    if (!file) return
+    const reader = new FileReader()
+    reader.addEventListener('load', function () {
+        imgPreview.src = this.result
+    }) 
+    reader.readAsDataURL(file)
+})
+
+pickerBtn.addEventListener('click', function () {
+    eyeDropper.open()
+    .then(res => {
+        result.innerHTML = `Tanlangan Rang: <b>${res.sRGBHex}</b>`
+    })
+    .catch(err => {
+        alert(`Foydalanuvchi tanlovni bekor qildi`);
+    })
+})
